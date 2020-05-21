196d934bcd219bbc138a3323d8ca69fba86aba8f
Imdeepmehta <iamdeepmehta25@gmail.com>
2020-05-21 19:08:58 +0530

    fix static bug

--- shortner/settings.py
+++ shortner/settings.py
@@ -111,13 +111,8 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static'),
+)
--- shortner/views.py
+++ shortner/views.py
@@ -8,31 +8,6 @@ from .generator import shortUrl
 def home(request):
     return render(request, 'home.html')
 
-
-# def generate(request):
-#     if request.method == "POST":
-#         url = request.POST['url']
-
-#         url_obj = Url()
-#         url_obj.url = url
-#         url_obj.save()
-
-#         print("Saved Sucessfully ")
-#         urls.addUrlsFromDb(url_obj)
-#         print( urls.addUrlsFromDb(url_obj))
-
-#         context = {
-#             "genratedUrl ": url_obj.url_id
-#         }
-#         print(str(context))
-#         return render(request, 'view.html', context=context)
-#     else:
-#         context = {
-#             "genratedUrl ": ""
-#         }
-#         return render(request, 'view.html', context=context)
-
-
 def generate(request):
     if request.method == "POST":
         url = request.POST['url']
